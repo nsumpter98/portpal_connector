ea3961b1dd3f2cb6cdae6b0e156fd40ec0f7efa4
Nathanael Sumpter <nsumpter98@gmail.com>
2025-06-29 00:10:06 -0400

    misc

--- main/vcp_usb_manager.cpp
+++ main/vcp_usb_manager.cpp
@@ -4,20 +4,6 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 #include "vcp_usb_manager.h"
-#include <stdio.h>
-#include <string.h>
-
-#include "esp_log.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
-
-#include "usb/cdc_acm_host.h"
-#include "usb/vcp_ch34x.hpp"
-#include "usb/vcp_cp210x.hpp"
-#include "usb/vcp_ftdi.hpp"
-#include "usb/vcp.hpp"
-#include "usb/usb_host.h"
 
 using namespace esp_usb;
 
--- main/vcp_usb_manager.h
+++ main/vcp_usb_manager.h
@@ -1,4 +1,18 @@
 #include "queue_manager.h"
+#include <stdio.h>
+#include <string.h>
+
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+
+#include "usb/cdc_acm_host.h"
+#include "usb/vcp_ch34x.hpp"
+#include "usb/vcp_cp210x.hpp"
+#include "usb/vcp_ftdi.hpp"
+#include "usb/vcp.hpp"
+#include "usb/usb_host.h"
 
 class vcp_usb_manager
 {
