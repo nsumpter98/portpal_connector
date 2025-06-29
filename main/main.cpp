713fc641a63ab43b8fe3c9f5b0724751c923a85c
Nathanael Sumpter <nsumpter98@gmail.com>
2025-06-28 22:36:46 -0400

    misc setup

--- main/main.cpp
+++ main/main.cpp
@@ -1,5 +1,16 @@
-void app_main(void){ 
-    while(1){
-        ESP_LOGE("hey")
+#include <esp_log.h>
+#include "esp_mac.h"
+
+namespace
+{
+    static const char *TAG = "MAIN";
+    extern "C" void app_main(void)
+    {
+        ESP_LOGI(TAG, "Entering main");
+        
+        while (1)
+        {
+            
+        }
     }
 }
