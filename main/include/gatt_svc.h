dbecea169e6a8999571886b6ee47b03022bb6f27
Nathanael Sumpter <nsumpter98@gmail.com>
2025-06-30 14:14:44 -0400

    working

---
+++ main/include/gatt_svc.h
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
+#ifndef GATT_SVR_H
+#define GATT_SVR_H
+
+/* Includes */
+#include "esp_gatts_api.h"
+
+
+/* Public function declarations */
+void send_heart_rate_indication(void);
+void gatt_svr_register_cb(struct ble_gatt_register_ctxt *ctxt, void *arg);
+void gatt_svr_subscribe_cb(struct ble_gap_event *event);
+int gatt_svc_init(void);
+
+#endif // GATT_SVR_H
