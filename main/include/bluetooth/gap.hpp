d389768f5fccc04b4237bcb689377600cf5a70f5
Nathanael Sumpter <nsumpter98@gmail.com>
2025-07-11 19:31:11 -0400

    working on gap controller

--- main/include/bluetooth/gap.hpp
+++ main/include/bluetooth/gap.hpp
@@ -12,10 +12,9 @@
 #define BLE_GAP_URI_PREFIX_HTTPS 0x17
 #define BLE_GAP_LE_ROLE_PERIPHERAL 0x00
 
-/* Public function declarations */
-
-void adv_init(void);
-int gap_init(void);
+static uint8_t own_addr_type;
+static uint8_t addr_val[6] = {0};
+static uint8_t esp_uri[16] = {BLE_GAP_URI_PREFIX_HTTPS, '/', '/', 'e', 's', 'p', 'r', 'e', 's', 's', 'i', 'f', '.', 'c', 'o', 'm'};
 
 // since we only plan to use portpal as a peripheral device I wont focus any on scanning.
 // currently referencing the following docs to build this out: https://docs.espressif.com/projects/esp-idf/en/v5.4.2/esp32s3/api-guides/ble/get-started/ble-introduction.html
@@ -24,12 +23,13 @@ class Gap
 public:
     Gap();
     void advertizing_init(void);
+    int gap_init(void);
 
 private:
-    void print_conn_desc(struct ble_gap_conn_desc *desc);
-    void format_addr(char *addr_str, uint8_t addr[]);
-    int gap_event_handler(struct ble_gap_event *event, void *arg);
-    void start_advertising(void);
+    static void print_conn_desc(struct ble_gap_conn_desc *desc);
+    static void format_addr(char *addr_str, uint8_t addr[]);
+    static int gap_event_handler(struct ble_gap_event *event, void *arg);
+    static void start_advertising(void);
 };
 
 #endif // GAP_SVC_H
